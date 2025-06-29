c0707bb082d6f55a2a1f0b58693a4d13a044ff2f
Arthur Sehnem <63933452+ArthurSehnem@users.noreply.github.com>
2025-06-29 19:33:27 -0300

    Update run_multi_xterm.sh

--- run_multi_xterm.sh
+++ run_multi_xterm.sh
@@ -1,16 +1,18 @@
-TOTAL_PROCESSES=3 
+#!/bin/bash
+
+TOTAL_PROCESSES=3
 
 echo "Lançando $TOTAL_PROCESSES processos MPI em terminais xterm separados..."
 
 mpirun -np $TOTAL_PROCESSES \
-       --bind-to none \
-       --mca orte_base_help_aggregate 0 \
-       --host localhost \
-       -x DISPLAY \
-       --map-by node \
-       --rank-by node \
-       --report-bindings \
-       --display-map \
-       --allow-run-as-root \
-       --oversubscribe \
-       ./launch_xterm_process.sh ./editor_colaborativo
+    --bind-to none \
+    --mca orte_base_help_aggregate 0 \
+    --host localhost \
+    -x DISPLAY \
+    --map-by node \
+    --rank-by node \
+    --report-bindings \
+    --display-map \
+    --allow-run-as-root \
+    --oversubscribe \
+    bash ./launch_xterm_process.sh ./editor_colaborativo
